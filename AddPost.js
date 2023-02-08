63101dbb85eaf5c3681067433dda5d87e0852886
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2023-02-08 20:46:06 +0530

    Add posts

--- AddPost.js
+++ AddPost.js
@@ -1,19 +1,83 @@
 const AllPost = [
   {
     category: "MMT",
-    Title: "Maximize Your Earnings: A Guide to Making Money Online in 2023 latest",
-    ImageUrl: "https://manoarya.com/Manoarya%20All%20Post/Images/HTML_basic_explanation.png",
+    Title: "100 Ways to Make Money Online: A Comprehensive Guide",
+    ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/100ways_to_make_money_online.png",
     PostUrl:
-      "../Manoarya%20All%20Post/Html-basic-explanation-by-manoarya.html",
-    alt: "html basic explanation by Manoarya",
+      "../Moneyaard_Post/100ways_to_make_money_online.html",
+    alt: "100 Ways to Make Money Online: A Comprehensive Guide",
   },
   {
     category: "MMT",
-    Title: "HTML basic explanation by Manoarya",
-    ImageUrl: "https://manoarya.com/Manoarya%20All%20Post/Images/HTML_basic_explanation.png",
+    Title: "E-Commerce: The Future of Shopping",
+    ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/ecommerce.png",
     PostUrl:
-      "../Manoarya%20All%20Post/Html-basic-explanation-by-manoarya.html",
-    alt: "html basic explanation by Manoarya",
+      "../Moneyaard_Post/ecommerce.html",
+    alt: "E-Commerce: The Future of Shopping",
   },
+  {
+      category: "MMT",
+      Title: "How to Make Money from E-Commerce: A Guide",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/Make%20Money%20from%20%20E-Commerce.png",
+      PostUrl:
+        "../Moneyaard_Post/How_to_Make_Money_from_E-Commerce_A_Guide.html",
+      alt: "How to Make Money from E-Commerce: A Guide",
+    },
+    {
+      category: "MMT",
+      Title: "how to make money from online courses and eBooks",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/online_course_ebooks.png",
+      PostUrl:
+        "../Moneyaard_Post/how_to_make_money_from_online_courses_and_eBooks.html",
+      alt: "how to make money from online courses and eBooks",
+    },
+    {
+      category: "MMT",
+      Title: "How to Make Money Online with Affiliate Marketing",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/How%20to%20Make%20Money%20Online%20with%20Affiliate%20Marketing.png",
+      PostUrl:
+        "../Moneyaard_Post/How_to_Make_Money_Online_with_Affiliate_Marketing.html",
+      alt: "How to Make Money Online with Affiliate Marketing",
+    },
+    {
+      category: "MMT",
+      Title: "Make Money from Dropshipping: A Step-by-Step Guide",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/dropshipping.png",
+      PostUrl:
+        "../Moneyaard_Post/Make_Money_from_Dropshipping_A_Step-by-Step_Guide.html",
+      alt: "Make Money from Dropshipping: A Step-by-Step Guide",
+    },
+    {
+      category: "MMT",
+      Title: "Maximize Your Earnings: A Guide to Making Money Online in 2023 latest",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/make_money_online.png",
+      PostUrl:
+        "../Moneyaard_Post/make_money_online.html",
+      alt: "Maximize Your Earnings: A Guide to Making Money Online in 2023 latest",
+    },
+    {
+      category: "MMT",
+      Title: "Making Money from Online Tutoring and Teaching: An Expert Guide",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/online_tutoring.png",
+      PostUrl:
+        "../Moneyaard_Post/Making_Money_from_Online_Tutoring_and_Teaching_An_Expert_Guide.html",
+      alt: "Making Money from Online Tutoring and Teaching: An Expert Guide",
+    },
+    {
+      category: "MMT",
+      Title: "Online Freelancing: A Comprehensive Guide to Making Money as a Freelancer",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/Online_Freelancing.png",
+      PostUrl:
+        "../Moneyaard_Post/Online_Freelancing_A_comprehensive_Guide_to_Making_Money_as_Freelancer.html",
+      alt: "Online Freelancing: A Comprehensive Guide to Making Money as a Freelancer",
+    },
+    {
+      category: "MMT",
+      Title: "Making Money from Surveys and Market Research: A Comprehensive Guide",
+      ImageUrl: "https://moneyaard.com/Moneyaard_Post/Images/surveys_market_research.png",
+      PostUrl:
+        "../Moneyaard_Post/Making_Money_from_Surveys_and_Market_Research_A_Comprehensive_Guide.html",
+      alt: "Making Money from Surveys and Market Research: A Comprehensive Guide",
+    },
 
 ]
